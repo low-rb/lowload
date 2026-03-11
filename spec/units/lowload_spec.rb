3f6f8817c73e5742fb40089506045b6c61544b10
maedi <maediprichard@gmail.com>
2026-03-11 21:14:43 +1100

    Add autoload

--- spec/units/lowload_spec.rb
+++ spec/units/lowload_spec.rb
@@ -3,6 +3,16 @@
 require_relative '../../lib/lowload'
 
 RSpec.describe LowLoad do
+  describe '.autoload' do
+    it 'loads a directory' do
+      LowLoad.autoload('spec/fixtures/directory')
+
+      expect(A).not_to be(nil)
+      expect(B).not_to be(nil)
+      expect(C).not_to be(nil)
+    end
+  end
+
   describe '.load' do
     it 'loads a file' do
       LowLoad.load('spec/fixtures/mock_node.rbx')
