ad2dcd61eb64dad96ba13e964fd8460f608d878a
maedi <maediprichard@gmail.com>
2026-04-11 14:06:48 +1000

    Move template parsing into template

--- lib/lowload.rb
+++ lib/lowload.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+require 'antlers'
 require 'lowkey'
+
 require_relative 'loader'
 
 def top_level_binding
@@ -53,17 +55,21 @@ module LowLoad
 
       # 3. "Runtime" phase (before LowNode instances are rendered).
       # Templates only exist if antlers gem has been required by another gem (such as LowNode) and the template contains antlers syntax.
-      templates.each do |namespace, template|
+      templates.each do |namespace, method_template|
         klass = const_get(namespace)
+        method, template = method_template
+        # TODO: If params contain "**props" or similar then send that so that lownode can replicate it.
+        params = method.params.map(&:name)
 
         next unless supports_templates?(klass)
 
-        klass.add_template(parser: Antlers, ast: Antlers.parse(template), template:, namespace:)
+        # TODO: Make template engine configurable.
+        klass.load_template(template:, params:, engine: Antlers, namespace:)
       end
     end
 
     def supports_templates?(klass)
-      klass.respond_to?(:render) && klass.respond_to?(:template) && klass.respond_to?(:add_template)
+      klass.respond_to?(:render) && klass.respond_to?(:template) && klass.respond_to?(:load_template)
     end
 
     def wrap_render_methods(file_proxy:)
@@ -78,7 +84,7 @@ module LowLoad
         render_method_body = render_method.body.export
 
         if defined?(Antlers) && ['{', '<{'].any? { |needle| render_method_body.include?(needle) }
-          templates[class_proxy.namespace] = render_method_body
+          templates[class_proxy.namespace] = [render_method, render_method_body]
         end
 
         # Intended to target HTML or Antlers tags. Use HEREDOC "<<~" in a plain Ruby (".rb") file, not RBX.
